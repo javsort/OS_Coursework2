c651fa333c51611bcbcf96334207eff526ea0b27
javsort <javierortega336@gmail.com>
2023-11-28 10:34:24 +0100

    first commit

---
+++ src/MemoryMgmt.java
@@ -0,0 +1,22 @@
+public class MemoryMgmt {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void malloc(int size){
+
+    }
+
+    public void free(int ptr){
+
+    }
+
+    public void sbrk(int size){
+
+    }
+
+    public void print(){
+        
+    }
+}
