e8c5ce496e194bbc84b959a27c60881fc0ec8847
javsort <javierortega336@gmail.com>
2023-12-06 13:17:42 +0100

    Input String Ready

--- src/MemoryMgmt.java
+++ src/MemoryMgmt.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.nio.BufferOverflowException;
 import java.util.*;
 
 /**
@@ -12,8 +12,8 @@ public class MemoryMgmt {
     // Memory size is 8192 bytes - 8KB
 
     // Variables for memory size
-    Integer absoluteSize = 8192;
-    Integer usableSize = 8192;
+    Integer absoluteSize;
+    Integer usableSize;
 
     static Integer ALLOC_HEADER = 8;
     static Integer FREE_HEADER = 16;
@@ -25,25 +25,37 @@ public class MemoryMgmt {
     String line = "";
 
     // Memory block lists - assign depending on size when free is called
-    public List<MemoryBlock> size32 = new ArrayList<>();
-    public List<MemoryBlock> size64 = new ArrayList<>();
-
-    public List<MemoryBlock> size512 = new ArrayList<>();
-    public List<MemoryBlock> size1024 = new ArrayList<>();
-
-    public List<MemoryBlock> size2048 = new ArrayList<>();
-    public List<MemoryBlock> size4096 = new ArrayList<>();
+    public List<MemoryBlock> size32;
+    public List<MemoryBlock> size64;
+    public List<MemoryBlock> size512;
+    public List<MemoryBlock> size1024; 
+    public List<MemoryBlock> size2048;
+    public List<MemoryBlock> size4096;
 
     // Initial Free List size 8192 bytes - 8KB
-    public List<MemoryBlock> size8192 = new ArrayList<>();
-
+    public List<MemoryBlock> size8192;
 
     // Free list - list of free lists spread by sizes
-    public List<List<MemoryBlock>> freeLists = new ArrayList<>();
+    public List<List<MemoryBlock>> freeLists;
 
-    // Constructor that initializes the lists to be used and the origianl FreeBlock
+    public List<UsedBlock> usedBlocks;
+
+    // CHECAR COMO FUNCIONA Y SI VALE LA PENA
+    public List<MemoryBlock> virtualMemory;
+
+    // Constructor that initializes the lists to be used
     public MemoryMgmt(){
-        // Initialize free lists and add them to the main list    
+        // Initialize free lists and add them to the main list 
+        size32 = new ArrayList<>();
+        size64 = new ArrayList<>();
+        size512 = new ArrayList<>();
+        size1024 = new ArrayList<>();
+        size2048 = new ArrayList<>();
+        size4096 = new ArrayList<>();
+        size8192 = new ArrayList<>();
+
+        freeLists = new ArrayList<>();
+        
         freeLists.add(size32);
         freeLists.add(size64);
         freeLists.add(size512);
@@ -52,17 +64,12 @@ public class MemoryMgmt {
         freeLists.add(size4096);
         freeLists.add(size8192);
 
-        // Initial full free block
-        FreeBlock OGfreeBlock = new FreeBlock(false, 0, true, 8192, null, null);
-
-        // Add the original free block to the 8KB list
-        size8192.add(OGfreeBlock);
+        usedBlocks = new ArrayList<>();
     }
 
     // Main Method
     public static void main(String[] args) {
         MemoryMgmt memory = new MemoryMgmt();
-
         memory.promptMenu();
 
     }
@@ -84,8 +91,7 @@ public class MemoryMgmt {
         
         // if null, call sbrk
         if(toUse == null){
-            System.out.println("Not enough memory to allocate. Call sbrk. (TO BE IMPLEMENTED)");
-            return -1;
+            throw new OutOfMemoryError("Not enough memory to allocate. Call sbrk. (TO BE IMPLEMENTED)");
         }
 
         // If yes, allocate through best fit
@@ -103,12 +109,9 @@ public class MemoryMgmt {
         // Add to free list
         addBackToFreeList(toUse);
 
-        String hex = String.format("0x%04x", Allocated.pointerReturned);
+        // String hex = String.format("0x%04x", Allocated.pointerReturned);
 
-        // DELETE AFTER TESTING
-        System.out.print("memory successfully allocated at " + Allocated.pointerReturned + ".\r\n");
-        
-        System.out.print("memory successfully allocated at " + hex + ".\r\n");
+        System.out.print("memory successfully allocated at " + getMeminHex(Allocated.pointerReturned) + ".\r\n\n");
 
         return Allocated.pointerReturned;
     }
@@ -173,8 +176,8 @@ public class MemoryMgmt {
      * Return nothing
      */
     public void free(int ptr){
-
         // Check if pointer is valid
+
 
         // Check if pointer is already free
 
@@ -192,6 +195,7 @@ public class MemoryMgmt {
      */
     public void sbrk(int size){
 
+        
     }
 
     /*
@@ -265,6 +269,7 @@ public class MemoryMgmt {
             super(FREE_HEADER, previousFree, previousSize, isFree, size);
             this.prev = pointer_prev;
             this.next = pointer_next;
+            this.totalsize = size - FREE_HEADER;
         }
 
         public int getSize(){
@@ -278,7 +283,6 @@ public class MemoryMgmt {
      */
     public class UsedBlock extends MemoryBlock {
         // USE ALLOC_HEADER = 8
-
         public int assignedSp[];
 
         public int pointerReturned;
@@ -288,30 +292,86 @@ public class MemoryMgmt {
 
             this.pointerReturned = previousSize + ALLOC_HEADER;
 
+            this.totalsize = size + ALLOC_HEADER;
             this.assignedSp = new int[size];
+
+            usedBlocks.add(this);
         }
 
-        /*public boolean fillMemory(String dataString){
-            // Get size of string in UTF-8 encoding
-            byte[] toUTF8 = dataString.getBytes(java.nio.charset.StandardCharsets.UTF_8);
-            int stringInBytes = toUTF8.length;
+        public boolean inputString(String dataString){
+            // Get size of string in ASCII - considering each ASCII character is 2 bytes
+            char[] toStore = dataString.toCharArray();
 
-            if(toUTF8.length > assignedSp.length){
-                System.out.println("String is too big to fit in memory block.");
+            int sizeInBytes = (toStore.length + 1) * 2; // +1 for null terminating char, *2 for 2 bytes per char
+
+            try {
+                if(sizeInBytes < size){
+                    // Store string in the assigned space
+                    for(int i = 0; i < toStore.length; i++){
+                        assignedSp[i] = (int) toStore[i];
+                    }
+                    assignedSp[toStore.length] = (int) '\0'; // Null terminating char
+
+                    // Update space and pointer
+                    size = size - assignedSp.length;
+                    pointerReturned = pointerReturned + assignedSp.length;
+
+                    return true;
+                } else {
+                    System.out.println("String is too big to fit in the allocated memory.");
+                    throw new BufferOverflowException();
+
+                }
+            } catch (BufferOverflowException e) {
+                System.out.println("Buffer Overflow Exception triggered. Please try again.");
+                Thread.currentThread().interrupt();
                 return false;
             }
+        }
 
-            return true;
-        }*/
-    }
+        public String getStoredString(){
+            String toReturn = "";
+
+            for(int i = 0; i < assignedSp.length; i++){
+                toReturn += (char) assignedSp[i];
+            }
+
+            return toReturn;
+        }
+
+        // to fill!
+        public int getDataInt(){
+            return this.assignedSp[0];
+        }
+
+        public int getPointer(){
+            return this.pointerReturned;
+        }
+    } 
 
     // run different tests and print results
     public void print(){
         switch (selectedOption) {
             case 1:
-                malloc(28);
-                malloc(1024);
-                malloc(28);
+                int pointer;
+                pointer = malloc(28);
+
+                pointer = inputDataString("Testing 1", pointer);
+
+                if(pointer < 0){
+                    break;
+                }
+                
+                try{
+                    System.out.println("Sleeping for 1 second... to simulate cool computing stuff. Think ab ur day for a second :)\n");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+
+                retrieveString(pointer);
+
+                free(pointer);
                 break;
         
             default:
@@ -320,13 +380,47 @@ public class MemoryMgmt {
         }
     }
 
+    public int inputDataString(String dataString, int pointer){
+        System.out.println("Storing the `" + dataString + "` string! At: " + getMeminHex(pointer) + " Please wait...\n");
+
+        for(UsedBlock current : usedBlocks){
+            if(current.pointerReturned == pointer){
+                if(current.inputString(dataString)){
+                    return current.pointerReturned;
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        System.out.println("Pointer not found. Please try again.");
+        return -1;
+    }
+
+    public void retrieveString(int pointer){
+        System.out.println("Retrieving data from pointer: " + getMeminHex(pointer) + ". Please wait...\n");
+
+        for(UsedBlock current : usedBlocks){
+            if(current.pointerReturned == pointer){
+                String storedString = current.getStoredString();
+                System.out.println("String retrieved: '" + storedString + "'\n");
+                return;
+            }
+        }
+
+        System.out.println("Pointer not found. Please try again.");
+    }
+
+
+
     /*
      * Prompt Menu
      * Prompt user to select a test to run
      */
     public void promptMenu(){
-        
         while(true){
+            cleanMemory();
+
             System.out.println("\r\n==================================");
             System.out.println("          Memory Manager");
             System.out.println("  Select a problem to initialize");
@@ -356,25 +450,50 @@ public class MemoryMgmt {
 
             switch(selectedOption){
                 case 1:
-                    System.out.println("Test 1 initializing...");
+                    System.out.println("Test 1 initializing...\n");
                     
 
                     print();
                     break;
                 case 2:
-                    System.out.println("Test 2 initializing...");
+                    System.out.println("Test 2 initializing...\n");
                     // set test characteristics
                     print();
                     break;
                 case 3:
-                    System.out.println("Test 3 initializing...");
+                    System.out.println("Test 3 initializing...\n");
                     // set test characteristics
                     print();
                     break;
                 default:
-                    System.out.println("Invalid selection. Bye!!!.");
+                    System.out.println("Invalid selection. Bye!!!.\n");
                     return;
             }
         }
+    }
+
+    public void cleanMemory(){
+        // Clear all lists
+        for(int i = 0; i < freeLists.size(); i++){
+            freeLists.get(i).clear();
+        }
+
+        // Clear used blocks
+        usedBlocks.clear();
+
+        // Variables for memory size
+        absoluteSize = 8192;
+        usableSize = 8192;
+
+        // Initial full free block
+        FreeBlock OGfreeBlock = new FreeBlock(false, 0, true, 8192, null, null);
+
+        // Add the original free block to the 8KB list
+        size8192.add(OGfreeBlock);
+    }
+
+    public String getMeminHex(int pointer){
+        String hex = String.format("0x%04x", pointer);
+        return hex;
     }
 }
